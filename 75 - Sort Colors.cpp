319010cf7c2b7bfa1eff77774131a3197208dd8d
Ali Zhussupov <zhussupovali@gmail.com>
2019-04-15 18:17:42 +0600

    leetcode: Solve 75 - Sort Colors

--- 75 - Sort Colors.cpp
+++ 75 - Sort Colors.cpp
@@ -4,31 +4,18 @@ public:
         int len = nums.size();
         if (len == 0)
             return;
-        int i = 0, l = 0, r = len - 1;
-        while (nums[l] == 0 && l < len - 1)
-            l++;
-        while (nums[r] == 2 && r > 0)
-            r--;
-        if (r == 0)
-            return;
-        i = l;
-        while (i <= r) {
-            if (nums[i] == 0) {
-                int temp = nums[l];
-                nums[l] = nums[i];
-                nums[i] = temp;
-                while (nums[l] == 0 && l < len - 1)
-                    l++;
+        int i = 0, j = 0;
+        for (int r = 0; r < len; r++) {
+            int v = nums[r];
+            nums[r] = 2;
+            if (v < 2) {
+                nums[j] = 1;
+                j++;
             }
-            if (nums[i] == 2) {
-                int temp = nums[r];
-                nums[r--] = nums[i];
-                nums[i] = temp;
-                while (nums[r] == 2 && r > 0)
-                    r--;
-            }
-            if (nums[i] == 1)
+            if (v == 0) {
+                nums[i] = 0;
                 i++;
+            }
         }
     }
 };
